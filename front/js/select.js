3666c90958f3093e0eae9eeda9229c7b7a9b3229
mikaup <muponen@gmail.com>
2019-09-11 11:48:15 +0300

    update

---
+++ front/js/select.js
@@ -0,0 +1,17 @@
+function vaihto() {
+let x = document.getElementById("select").value;
+if (x === "playerElement1") {
+console.log("1")
+}else if (x === "playerElement2"){
+  console.log("2")
+}else if (x === "playerElement3"){
+  console.log("3")
+}else if (x === "playerElement4"){
+  console.log("4")
+}else {
+  console.log("not working");
+  console.log(x);
+}
+
+}
+
